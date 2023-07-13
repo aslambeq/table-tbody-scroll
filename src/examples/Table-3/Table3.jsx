17e1f2fbb464eee63b29c626c88ba7fba3e59fd1
aslambeq <68230863+aslambeq@users.noreply.github.com>
2023-07-13 02:27:19 +0000

    fix: validateDOMNesting

--- src/examples/Table-3/Table3.jsx
+++ src/examples/Table-3/Table3.jsx
@@ -3,135 +3,143 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
-  useReactTable
-} from '@tanstack/react-table'
-import { useReducer, useState } from 'react'
-import useFakeData from './useFakeData'
-import './Table3.css'
+  useReactTable,
+} from "@tanstack/react-table";
+import { useReducer, useState } from "react";
+import useFakeData from "./useFakeData";
+import "./Table3.css";
 
-const columnHelper = createColumnHelper()
+const columnHelper = createColumnHelper();
 
 const columns = [
-  columnHelper.accessor('firstName', {
-    width: '100%',
+  columnHelper.accessor("firstName", {
+    width: "100%",
     cell: (info) => info.getValue(),
     footer: (info) => {
-      return info.column.id
-    }
+      return info.column.id;
+    },
   }),
   columnHelper.accessor((row) => row.lastName, {
     // width: '100%',
-    id: 'lastName',
+    id: "lastName",
     cell: (info) => <i>{info.getValue()}</i>,
     header: () => <span>Last Name</span>,
-    footer: (info) => info.column.id
+    footer: (info) => info.column.id,
   }),
-  columnHelper.accessor('age', {
-    width: '60%',
-    header: () => 'Age',
+  columnHelper.accessor("age", {
+    width: "60%",
+    header: () => "Age",
     cell: (info) => info.renderValue(),
-    footer: (info) => info.column.id
+    footer: (info) => info.column.id,
   }),
-  columnHelper.accessor('visits', {
-    width: '60%',
+  columnHelper.accessor("visits", {
+    width: "60%",
     header: () => <span>Visits</span>,
-    footer: (info) => info.column.id
+    footer: (info) => info.column.id,
   }),
-  columnHelper.accessor('status', {
+  columnHelper.accessor("status", {
     // width: '100%',
-    header: 'Status',
-    footer: (info) => info.column.id
+    header: "Status",
+    footer: (info) => info.column.id,
   }),
-  columnHelper.accessor('progress', {
+  columnHelper.accessor("progress", {
     // width: '100%',
-    header: 'Profile Progress',
-    footer: (info) => info.column.id
-  })
-]
+    header: "Profile Progress",
+    footer: (info) => info.column.id,
+  }),
+];
 
 const Table3 = () => {
-  const { users } = useFakeData()
-  const [data, setData] = useState(() => [...users])
+  const { users } = useFakeData();
+  const [data, setData] = useState(() => [...users]);
 
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel()
-  })
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
-    <table className='table3'>
-      <div className='table-title'>
-        <caption>Users list</caption>
+    <div className="table3">
+      <div className="table-title">
+        <table>
+          <caption>Users list</caption>
+        </table>
       </div>
-      <div className='table-head'>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
+      <div className="table-head">
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      style={{ width: header.column.columnDef.width || "100%" }}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+        </table>
+      </div>
+      <div className="table-body">
+        <table>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td
+                      key={cell.id}
+                      style={{
+                        width: cell.column.columnDef.width || "100%",
+                      }}
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="table-footer">
+        <table>
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
                   <th
                     key={header.id}
-                    style={{ width: header.column.columnDef.width || '100%' }}
+                    style={{
+                      width: header.column.columnDef.width || "100%",
+                    }}
                   >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
-                          header.column.columnDef.header,
+                          header.column.columnDef.footer,
                           header.getContext()
                         )}
                   </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        </table>
       </div>
-      <div className='table-body'>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td
-                    key={cell.id}
-                    style={{
-                      width: cell.column.columnDef.width || '100%'
-                    }}
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                )
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </div>
-      <div className='table-footer'>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  style={{
-                    width: header.column.columnDef.width || '100%'
-                  }}
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </div>
-    </table>
-  )
-}
+    </div>
+  );
+};
 
-export default Table3
+export default Table3;
